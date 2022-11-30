ed79be44358f3b5b79030d3a4c8b0d28b9248e09
lexpin23 <lesly_jpf_jnp@hotmail.com>
2022-11-30 00:08:42 -0700

    Declarando variables State

--- ContentView.swift
+++ ContentView.swift
@@ -8,5 +8,34 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-   
+    let CoreDM: CoreDataManager
+    @State var id=""
+    @State var nombre=""
+    @State var precio=""
+    @State var existencia=""
+    @State var categoria=""
+    @State var seleccionado:Mobiliario?
+    @State var moArray=[Mobiliario]()
+    
+    var body: some View {
+        VStack{
+            TextField("Id",         text: $id)          .textFieldStyle(RoundedBorderTextFieldStyle())
+            TextField("Nombre",     text: $nombre)      .textFieldStyle(RoundedBorderTextFieldStyle())
+            TextField("Precio",     text: $precio)      .textFieldStyle(RoundedBorderTextFieldStyle())
+            TextField("Existencia", text: $existencia)  .textFieldStyle(RoundedBorderTextFieldStyle())
+            TextField("Categoria",  text: $categoria)   .textFieldStyle(RoundedBorderTextFieldStyle())
+            Button("Save"){
+                if(seleccionado != nil){
+                    seleccionado?.id=id
+                    seleccionado?.nombre=nombre
+                    seleccionado?.precio=precio
+                    seleccionado?.existencia=existencia
+                    seleccionado?.categoria=categoria
+                    CoreDM.actualizarMobiliario(mobiliario: seleccionado!)
+                }else{
+                    CoreDM.guardarMobiliario(id: id, nombre: nombre, precio: precio, existencia: existencia, categoria: categoria)
+                }
+            }
+        }   
+    }
 }

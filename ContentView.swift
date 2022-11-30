b6292a6f6ffad9b71d70c84ed3b01569df1fa775
lexpin23 <lesly_jpf_jnp@hotmail.com>
2022-11-29 23:58:42 -0700

    Corrigiendo archivo ContentView
    Me equivoqué de archivo, estaba llenando código en uno que no va

--- ContentView.swift
+++ ContentView.swift
@@ -4,36 +4,9 @@
 //
 //  Created by Lokura on 29/11/22.
 //
-
 import SwiftUI
 import CoreData
 
-class CoreDataManager{
-    let persistentContainer : NSPersistentContainer
-    init(){
-        persistentContainer = NSPersistentContainer(name: "Mobiliario")
-        persistentContainer.loadPersistentStores(completionHandler: {
-            (descripcion, error) in
-            if let error = error {
-                fatalError("core data failed to inicialice \(error.localizedDescription)")
-            }
-        })
-    }
-
-    func guardarMobiliario(id:String, nombre:String, precio:String, existencia:String, categoria:String){
-        let mobiliario = Mobiliario(context : persistentContainer.viewContext)
-        mobiliario.id = id
-        mobiliario.nombre=nombre
-        mobiliario.precio=precio
-        mobiliario.existencia=existencia
-        mobiliario.categoria = categoria
-        
-        do{
-            try persistentContainer.viewContext.save()
-            print("Guardado con éxito")
-        }
-        catch{
-            print("failed to  save error en \(error)")
-        }
-    }
+struct ContentView: View {
+   
 }
---
+++ CoreDataManager.swift
@@ -0,0 +1,42 @@
+import SwiftUI
+import CoreData
+
+class CoreDataManager{
+    let persistentContainer : NSPersistentContainer
+    init(){
+        persistentContainer = NSPersistentContainer(name: "Mobiliario")
+        persistentContainer.loadPersistentStores(completionHandler: {
+            (descripcion, error) in
+            if let error = error {
+                fatalError("core data failed to inicialice \(error.localizedDescription)")
+            }
+        })
+    }
+
+    func guardarMobiliario(id:String, nombre:String, precio:String, existencia:String, categoria:String){
+        let mobiliario = Mobiliario(context : persistentContainer.viewContext)
+        mobiliario.id = id
+        mobiliario.nombre=nombre
+        mobiliario.precio=precio
+        mobiliario.existencia=existencia
+        mobiliario.categoria = categoria
+        
+        do{
+            try persistentContainer.viewContext.save()
+            print("Guardado con éxito")
+        }
+        catch{
+            print("Error al guardar. Error en \(error)")
+        }
+    }
+
+    func leerTodoMobiliario()-> [Mobiliario]{
+        let fetchRequest:NSFetchRequest<Mobiliario>=Mobiliario.fetchRequest()
+        do {
+            return try persistentContainer.viewContext.fetch(fetchRequest)
+        }
+        catch{
+            return[]
+        }
+    }
+}
